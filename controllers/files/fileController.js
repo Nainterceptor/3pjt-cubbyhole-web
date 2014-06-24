8ddefa7dd905a40e22c8d169f52ef30d0d57c229
Gaël Demette <demette.gael@gmail.com>
2014-06-24 13:52:32 +0200

    Files editing

--- controllers/files/fileController.js
+++ controllers/files/fileController.js
@@ -18,8 +18,33 @@ exports.removeFile = function (req, res) {
 };
 
 exports.getEditFile = function(req, res) {
+    var headers = {token: req.cookies.token};
+    unirest
+        .get(config.api + '/file/get/' + req.params.file)
+        .headers(headers)
+        .end(function(rest) {
+            var response = rest.body;
+            res.render('files/editFile.html.twig', response.file);
+        });
 };
 
 exports.postEditFile = function(req, res) {
+    var output = req.body;
+    var url = config.api + '/file/update/' + req.params.file + '/edit-rights';
 
+    unirest
+        .post(url)
+        .type('json')
+        .send(output)
+        .headers({token: req.cookies.token})
+        .end(function (rest) {
+                    var response = rest.body;
+                    if (response.success != true) {
+                        res.flashBag.add("danger", "Something failed !");
+                        res.render('files/editFile.html.twig', output);
+                    } else {
+                        res.flashBag.add("success", "File successful updated !");
+                        res.redirect('/user/webapp');
+                    }
+        });
 };
